ea32262f2571a008dbb03eb9830e394655ca3335
TwinkleGalaxy <46693515+TwinkleGalaxy@users.noreply.github.com>
2019-01-25 18:48:39 +0200

    Update Program.cs

--- ToDoListApp/Program.cs
+++ ToDoListApp/Program.cs
@@ -19,7 +19,7 @@ namespace ToDoListApp
 
             while (true) 
             {
-                Console.Write("\nPlease enter your name: ");
+                Console.Write("\nPlease enter your username: ");
                 username = Console.ReadLine();
                 User myUser = new User();
                 ToDoList toDoList = new ToDoList();
